727dcff5e7edc8ce43de09b9afdf111955eee134
Nuwan-alt <jayasankanuwan1998@gmail.com>
2022-10-14 02:22:50 +0530

    first commit

---
+++ leaderboard/data.js
@@ -0,0 +1,875 @@
+function getNames(){
+return ([
+    {
+        "Name": "InfernoSquad_MX6",
+        "H_username": "InfernoSquad_MX6"
+    },
+    {
+        "Name": "ABot_MX6",
+        "H_username": "ABot_MX6"
+    },
+    {
+        "Name": "byteCallers_MX6",
+        "H_username": "byteCallers_MX6"
+    },
+    {
+        "Name": "Heisen_MX6",
+        "H_username": "Heisen_MX6"
+    },
+    {
+        "Name": "GAP_MX6",
+        "H_username": "GAP_MX6"
+    },
+    {
+        "Name": "TwisterBoot_MX6",
+        "H_username": "TwisterBoot_MX6"
+    },
+    {
+        "Name": "Team_TECO_MX6",
+        "H_username": "Team_TECO_MX6"
+    },
+    {
+        "Name": "Lumos_MX6",
+        "H_username": "Lumos_MX6"
+    },
+    {
+        "Name": "Impostors_MX6",
+        "H_username": "Impostors_MX6"
+    },
+    {
+        "Name": "TeamPK_MX6",
+        "H_username": "TeamPK_MX6"
+    },
+    {
+        "Name": "Matrix_MX6",
+        "H_username": "Matrix_MX6"
+    },
+    {
+        "Name": "CodeRiders_MX6",
+        "H_username": "CodeRiders_MX6"
+    },
+    {
+        "Name": "Galaxy_MX6",
+        "H_username": "Galaxy_MX6"
+    },
+    {
+        "Name": "ToitCoderz_MX6",
+        "H_username": "ToitCoderz_MX6"
+    },
+    {
+        "Name": "ExpelliCodus_MX6",
+        "H_username": "ExpelliCodus_MX6"
+    },
+    {
+        "Name": "CRACK_MX6",
+        "H_username": "CRACK_MX6"
+    },
+    {
+        "Name": "ThreeAngles_MX6",
+        "H_username": "ThreeAngles_MX6"
+    },
+    {
+        "Name": "Turbo Dynamics_MX6",
+        "H_username": "Turbo Dynamics_MX6"
+    },
+    {
+        "Name": "BugBusters_MX6",
+        "H_username": "BugBusters_MX6"
+    },
+    {
+        "Name": "500-server-error_MX6",
+        "H_username": "500-server-error_MX6"
+    },
+    {
+        "Name": "MTwins_MX6",
+        "H_username": "MTwins_MX6"
+    },
+    {
+        "Name": "#dec0ders_MX6",
+        "H_username": "#dec0ders_MX6"
+    },
+    {
+        "Name": "ThunderBolt_MX6",
+        "H_username": "ThunderBolt_MX6"
+    },
+    {
+        "Name": "Alpha team_MX6",
+        "H_username": "Alpha team_MX6"
+    },
+    {
+        "Name": "xCore_MX6",
+        "H_username": "xCore_MX6"
+    },
+    {
+        "Name": "Fathima_Shazina_MX6",
+        "H_username": "Fathima_Shazina_MX6"
+    },
+    {
+        "Name": "Gamma_MX6",
+        "H_username": "Gamma_MX6"
+    },
+    {
+        "Name": "TADA_MX6",
+        "H_username": "TADA_MX6"
+    },
+    {
+        "Name": "Team TrioBUM_MX6",
+        "H_username": "Team TrioBUM_MX6"
+    },
+    {
+        "Name": "CovidCoders_MX6",
+        "H_username": "CovidCoders_MX6"
+    },
+    {
+        "Name": "RootNameErr_MX6",
+        "H_username": "RootNameErr_MX6"
+    },
+    {
+        "Name": "White_Devils_MX6",
+        "H_username": "White_Devils_MX6"
+    },
+    {
+        "Name": "Team404_MX6",
+        "H_username": "Team404_MX6"
+    },
+    {
+        "Name": "RedHunters_MX6",
+        "H_username": "RedHunters_MX6"
+    },
+    {
+        "Name": "TeamX_MX6",
+        "H_username": "TeamX_MX6"
+    },
+    {
+        "Name": "eyeCoders_MX6",
+        "H_username": "eyeCoders_MX6"
+    },
+    {
+        "Name": "Radianz_MX6",
+        "H_username": "Radianz_MX6"
+    },
+    {
+        "Name": "AlphaQ_MX6",
+        "H_username": "AlphaQ_MX6"
+    },
+    {
+        "Name": "white_shark_MX6",
+        "H_username": "white_shark_MX6"
+    },
+    {
+        "Name": "AlphaPack_MX6",
+        "H_username": "AlphaPack_MX6"
+    },
+    {
+        "Name": "CODERZ_SQUAD_MX6",
+        "H_username": "CODERZ_SQUAD_MX6"
+    },
+    {
+        "Name": "ExTop_MX6",
+        "H_username": "ExTop_MX6"
+    },
+    {
+        "Name": "Decoders_MX6",
+        "H_username": "Decoders_MX6"
+    },
+    {
+        "Name": "Time_MX6",
+        "H_username": "Time_MX6"
+    },
+    {
+        "Name": "Codetrix_MX6",
+        "H_username": "Codetrix_MX6"
+    },
+    {
+        "Name": "Wiresharks_MX6",
+        "H_username": "Wiresharks_MX6"
+    },
+    {
+        "Name": "TeamABC_MX6",
+        "H_username": "TeamABC_MX6"
+    },
+    {
+        "Name": "SAID_MX6",
+        "H_username": "SAID_MX6"
+    },
+    {
+        "Name": "ExpelliCodus_MX6",
+        "H_username": "ExpelliCodus_MX6"
+    },
+    {
+        "Name": "Beno1_MX6",
+        "H_username": "Beno1_MX6"
+    },
+    {
+        "Name": "Kyojin_MX6",
+        "H_username": "Kyojin_MX6"
+    },
+    {
+        "Name": "MDM_MX6",
+        "H_username": "MDM_MX6"
+    },
+    {
+        "Name": "TeamSync_MX6",
+        "H_username": "TeamSync_MX6"
+    },
+    {
+        "Name": "CodeZ_MX6",
+        "H_username": "CodeZ_MX6"
+    },
+    {
+        "Name": "RAGE_MX6",
+        "H_username": "RAGE_MX6"
+    },
+    {
+        "Name": "Uranometria_MX6",
+        "H_username": "Uranometria_MX6"
+    },
+    {
+        "Name": "404Found_MX6",
+        "H_username": "404Found_MX6"
+    },
+    {
+        "Name": "codeJitsu_MX6",
+        "H_username": "codeJitsu_MX6"
+    },
+    {
+        "Name": "CodeBullets_MX6",
+        "H_username": "CodeBullets_MX6"
+    },
+    {
+        "Name": "TechFlow_MX6",
+        "H_username": "TechFlow_MX6"
+    },
+    {
+        "Name": "Revolt_MX6",
+        "H_username": "Revolt_MX6"
+    },
+    {
+        "Name": "XtremeNoobs_MX6",
+        "H_username": "XtremeNoobs_MX6"
+    },
+    {
+        "Name": "Triggermindscape_MX6",
+        "H_username": "Triggermindscape_MX6"
+    },
+    {
+        "Name": "Nova_MX6",
+        "H_username": "Nova_MX6"
+    },
+    {
+        "Name": "DIO_MX6",
+        "H_username": "DIO_MX6"
+    },
+    {
+        "Name": "CodeGen_MX6",
+        "H_username": "CodeGen_MX6"
+    },
+    {
+        "Name": "Pentagon_MX6",
+        "H_username": "Pentagon_MX6"
+    },
+    {
+        "Name": "team_cnys_MX6",
+        "H_username": "team_cnys_MX6"
+    },
+    {
+        "Name": "JustStarted_MX6",
+        "H_username": "JustStarted_MX6"
+    },
+    {
+        "Name": "BugHunters_MX6",
+        "H_username": "BugHunters_MX6"
+    },
+    {
+        "Name": "Code360_MX6",
+        "H_username": "Code360_MX6"
+    },
+    {
+        "Name": "PPG_MX6",
+        "H_username": "PPG_MX6"
+    },
+    {
+        "Name": "Hexclan_MX6",
+        "H_username": "Hexclan_MX6"
+    },
+    {
+        "Name": "IWnt2StyHere_MX6",
+        "H_username": "IWnt2StyHere_MX6"
+    },
+    {
+        "Name": "Strikers_MX6",
+        "H_username": "Strikers_MX6"
+    },
+    {
+        "Name": "Cringans_MX6",
+        "H_username": "Cringans_MX6"
+    },
+    {
+        "Name": "CodeClan_404_MX6",
+        "H_username": "CodeClan_404_MX6"
+    },
+    {
+        "Name": "Bellatrix_MX6",
+        "H_username": "Bellatrix_MX6"
+    },
+    {
+        "Name": "hexClan_MX6",
+        "H_username": "hexClan_MX6"
+    },
+    {
+        "Name": "Andromeda_MX6",
+        "H_username": "Andromeda_MX6"
+    },
+    {
+        "Name": "Think_MX6",
+        "H_username": "Think_MX6"
+    },
+    {
+        "Name": "n00bCod3rs_MX6",
+        "H_username": "n00bCod3rs_MX6"
+    },
+    {
+        "Name": "WHS_MX6",
+        "H_username": "WHS_MX6"
+    },
+    {
+        "Name": "AlgoReapers_MX6",
+        "H_username": "AlgoReapers_MX6"
+    },
+    {
+        "Name": "Ravana_MX6",
+        "H_username": "Ravana_MX6"
+    },
+    {
+        "Name": "vipoothi_MX6",
+        "H_username": "vipoothi_MX6"
+    },
+    {
+        "Name": "33KDP_MX6",
+        "H_username": "33KDP_MX6"
+    },
+    {
+        "Name": "TAT_MX6",
+        "H_username": "TAT_MX6"
+    },
+    {
+        "Name": "SandN_MX6",
+        "H_username": "SandN_MX6"
+    },
+    {
+        "Name": "WHS_MX6",
+        "H_username": "WHS_MX6"
+    },
+    {
+        "Name": "Pico-roid_MX6",
+        "H_username": "Pico-roid_MX6"
+    },
+    {
+        "Name": "SP4G_chiMera_MX6",
+        "H_username": "SP4G_chiMera_MX6"
+    },
+    {
+        "Name": "TeamViKi_MX6",
+        "H_username": "teamviki98_MX6"
+    },
+    {
+        "Name": "CodeError_MX6",
+        "H_username": "CodeError_MX6"
+    },
+    {
+        "Name": "All_Stars_MX6",
+        "H_username": "All_Stars_MX6"
+    },
+    {
+        "Name": "Andromeda15_MX6",
+        "H_username": "Andromeda15_MX6"
+    },
+    {
+        "Name": "Dark_devils_MX6",
+        "H_username": "Dark_devils_MX6"
+    },
+    {
+        "Name": "Titan_MX6",
+        "H_username": "Titan_MX6"
+    },
+    {
+        "Name": "Enigma_MX6",
+        "H_username": "Enigma_MX6"
+    },
+    {
+        "Name": "Codebusters_MX6",
+        "H_username": "Codebusters_MX6"
+    },
+    {
+        "Name": "Codegenesse_MX6",
+        "H_username": "Codegenesse_MX6"
+    },
+    {
+        "Name": "Shark Boys_MX6",
+        "H_username": "Shark Boys_MX6"
+    },
+    {
+        "Name": "Shark Boy 01_MX6",
+        "H_username": "Shark Boy 01_MX6"
+    },
+    {
+        "Name": "dataPirates_MX6",
+        "H_username": "dataPirates_MX6"
+    },
+    {
+        "Name": "3bits_MX6",
+        "H_username": "3bits_MX6"
+    },
+    {
+        "Name": "CodeRNA_MX6",
+        "H_username": "CodeRNA_MX6"
+    },
+    {
+        "Name": "Weirdos_MX6",
+        "H_username": "Weirdos_MX6"
+    },
+    {
+        "Name": "CodeX_MX6",
+        "H_username": "CodeX_MX6"
+    },
+    {
+        "Name": "debugTribe_MX6",
+        "H_username": "debugTribe_MX6"
+    },
+    {
+        "Name": "ESKIMOS_MX6",
+        "H_username": "ESKIMOS_MX6"
+    },
+    {
+        "Name": "CodEBlacK_MX6",
+        "H_username": "CodEBlacK_MX6"
+    },
+    {
+        "Name": "Code makers_MX6",
+        "H_username": "Code makers_MX6"
+    },
+    {
+        "Name": "semicolon3_MX6",
+        "H_username": "semicolon3_MX6"
+    },
+    {
+        "Name": "Terminators_MX6",
+        "H_username": "Terminators_MX6"
+    },
+    {
+        "Name": "Codecrew_MX6",
+        "H_username": "Codecrew_MX6"
+    },
+    {
+        "Name": "0_o_MX6",
+        "H_username": "0_o_MX6"
+    },
+    {
+        "Name": "DedSecGMX6",
+        "H_username": "DedSecGMX6"
+    },
+    {
+        "Name": "AmateurX_MX6",
+        "H_username": "AmateurX_MX6"
+    },
+    {
+        "Name": "CodeCHeckers_MX6",
+        "H_username": "CodeCHeckers_MX6"
+    },
+    {
+        "Name": "d3cod3monk_MX6",
+        "H_username": "d3cod3monk_MX6"
+    },
+    {
+        "Name": "Coding_Angels_MX6",
+        "H_username": "Coding_Angels_MX6"
+    },
+    {
+        "Name": "Bug Busters_MX6",
+        "H_username": "Bug Busters_MX6"
+    },
+    {
+        "Name": "Etcetera_MX6",
+        "H_username": "Etcetera_MX6"
+    },
+    {
+        "Name": "SemicolonSLK_MX6",
+        "H_username": "SemicolonSLK_MX6"
+    },
+    {
+        "Name": "Avengers_MX6",
+        "H_username": "Avengers_MX6"
+    },
+    {
+        "Name": "SP4G_TriWizarDs_MX6",
+        "H_username": "SP4G_TriWizarDs_MX6"
+    },
+    {
+        "Name": "IRIS_MX6",
+        "H_username": "IRIS_MX6"
+    },
+    {
+        "Name": "Team Falcon_MX6",
+        "H_username": "Team Falcon_MX6"
+    },
+    {
+        "Name": "TechMates_MX6",
+        "H_username": "TechMates_MX6"
+    },
+    {
+        "Name": "Loopers_MX6",
+        "H_username": "Loopers_MX6"
+    },
+    {
+        "Name": "CoolCoders_MX6",
+        "H_username": "CoolCoders_MX6"
+    },
+    {
+        "Name": "Felix_MX6",
+        "H_username": "Felix_MX6"
+    },
+    {
+        "Name": "Newwera_MX6",
+        "H_username": "Newwera_MX6"
+    },
+    {
+        "Name": "Code_Stars_MX6",
+        "H_username": "Code_Stars_MX6"
+    },
+    {
+        "Name": "averageGuys_MX6",
+        "H_username": "averageGuys_MX6"
+    },
+    {
+        "Name": "Hack slash_MX6",
+        "H_username": "Hack slash_MX6"
+    },
+    {
+        "Name": "DoubleZero_MX6",
+        "H_username": "DoubleZero_MX6"
+    },
+    {
+        "Name": "Team Baymax_MX6",
+        "H_username": "Team Baymax_MX6"
+    },
+    {
+        "Name": "sOLO_MX6",
+        "H_username": "sOLO_MX6"
+    },
+    {
+        "Name": "SECoders_MX6",
+        "H_username": "SECoders_MX6"
+    },
+    {
+        "Name": "Solo_MX6",
+        "H_username": "Solo_MX6"
+    },
+    {
+        "Name": "kadalaBytes_MX6",
+        "H_username": "kadalaBytes_MX6"
+    },
+    {
+        "Name": "clueless_MX6",
+        "H_username": "clueless_MX6"
+    },
+    {
+        "Name": "Debuggers_MX6",
+        "H_username": "Debuggers_MX6"
+    },
+    {
+        "Name": "IntElligence_MX6",
+        "H_username": "IntElligence_MX6"
+    },
+    {
+        "Name": "Pegasus_MX6",
+        "H_username": "Pegasus_MX6"
+    },
+    {
+        "Name": "RuleBreakers_MX6",
+        "H_username": "RuleBreakers_MX6"
+    },
+    {
+        "Name": "BinaryDragons_MX6",
+        "H_username": "BinaryDragons_MX6"
+    },
+    {
+        "Name": "FobidnLinker_MX6",
+        "H_username": "FobidnLinker_MX6"
+    },
+    {
+        "Name": "Believers_MX6",
+        "H_username": "Believers_MX6"
+    },
+    {
+        "Name": "Enigma_MX6",
+        "H_username": "Enigma_MX6"
+    },
+    {
+        "Name": "Team_MX6",
+        "H_username": "Team_MX6"
+    },
+    {
+        "Name": "XCodeX_MX6",
+        "H_username": "XCodeX_MX6"
+    },
+    {
+        "Name": "SharkX_MX6",
+        "H_username": "SharkX_MX6"
+    },
+    {
+        "Name": "Girlz_MX6",
+        "H_username": "Girlz_MX6"
+    },
+    {
+        "Name": "Yes_I_Can_MX6",
+        "H_username": "Yes_I_Can_MX6"
+    },
+    {
+        "Name": "CdWarriors_MX6",
+        "H_username": "CdWarriors_MX6"
+    },
+    {
+        "Name": "Jehan_MX6",
+        "H_username": "Jehan_MX6"
+    },
+    {
+        "Name": "TeamEnigma_MX6",
+        "H_username": "TeamEnigma_MX6"
+    },
+    {
+        "Name": "GenetriX_MX6",
+        "H_username": "GenetriX_MX6"
+    },
+    {
+        "Name": "ParticipationOnly_MX6",
+        "H_username": "ParticipationOnly_MX6"
+    },
+    {
+        "Name": "Peak_Finders_MX6",
+        "H_username": "Peak_Finders_MX6"
+    },
+    {
+        "Name": "RuntimeWarriors_MX6",
+        "H_username": "RuntimeWarriors_MX6"
+    },
+    {
+        "Name": "Code Unity_MX6",
+        "H_username": "Code Unity_MX6"
+    },
+    {
+        "Name": "Three_20s_MX6",
+        "H_username": "Three_20s_MX6"
+    },
+    {
+        "Name": "ItCal_MX6",
+        "H_username": "ItCal_MX6"
+    },
+    {
+        "Name": "Conquerors_MX6",
+        "H_username": "Conquerors_MX6"
+    },
+    {
+        "Name": "CodeBlack_MX6",
+        "H_username": "CodeBlack_MX6"
+    },
+    {
+        "Name": "sapnu_MX6",
+        "H_username": "sapnu_MX6"
+    },
+    {
+        "Name": "Papyrus_MX6",
+        "H_username": "Papyrus_MX6"
+    },
+    {
+        "Name": "CRU_MX6",
+        "H_username": "CRU_MX6"
+    },
+    {
+        "Name": "Team_Dragons_MX6",
+        "H_username": "Team_Dragons_MX6"
+    },
+    {
+        "Name": "DragonCoders_MX6",
+        "H_username": "DragonCoders_MX6"
+    },
+    {
+        "Name": "Automics_MX6",
+        "H_username": "Automics_MX6"
+    },
+    {
+        "Name": "StarTechies_MX6",
+        "H_username": "StarTechies_MX6"
+    },
+    {
+        "Name": "Wolves_MX6",
+        "H_username": "Wolves_MX6"
+    },
+    {
+        "Name": "Genisis_MX6",
+        "H_username": "Genisis_MX6"
+    },
+    {
+        "Name": "STN-BOYS_MX6",
+        "H_username": "STN-BOYS_MX6"
+    },
+    {
+        "Name": "Acies_MX6",
+        "H_username": "Acies_MX6"
+    },
+    {
+        "Name": "CD_MX6",
+        "H_username": "CD_MX6"
+    },
+    {
+        "Name": "Rini_MX6",
+        "H_username": "Rini_MX6"
+    },
+    {
+        "Name": "paanBatta_MX6",
+        "H_username": "paanBatta_MX6"
+    },
+    {
+        "Name": "BVN coders_MX6",
+        "H_username": "BVN coders_MX6"
+    },
+    {
+        "Name": "Trojan_MX6",
+        "H_username": "Trojan_MX6"
+    },
+    {
+        "Name": "Sneaky Robots_MX6",
+        "H_username": "Sneaky Robots_MX6"
+    },
+    {
+        "Name": "Pandora_MX6",
+        "H_username": "Pandora_MX6"
+    },
+    {
+        "Name": " Pandora_MX6",
+        "H_username": " Pandora_MX6"
+    },
+    {
+        "Name": "Duellists_MX6",
+        "H_username": "Duellists_MX6"
+    },
+    {
+        "Name": "NoGPA_MX6",
+        "H_username": "NoGPA_MX6"
+    },
+    {
+        "Name": "kottuGang_MX6",
+        "H_username": "kottuGang_MX6"
+    },
+    {
+        "Name": "NeMeSis_MX6",
+        "H_username": "NeMeSis_MX6"
+    },
+    {
+        "Name": "Team-404_MX6",
+        "H_username": "Team-404_MX6"
+    },
+    {
+        "Name": "Syntax_Error_MX6",
+        "H_username": "Syntax_Error_MX6"
+    },
+    {
+        "Name": "H2R_MX6",
+        "H_username": "H2R_MX6"
+    },
+    {
+        "Name": "ZenMTK_MX6",
+        "H_username": "ZenMTK_MX6"
+    },
+    {
+        "Name": "Code_Ai_MX6",
+        "H_username": "Code_Ai_MX6"
+    },
+    {
+        "Name": "Bit_by_Bit_MX6",
+        "H_username": "Bit_by_Bit_MX6"
+    },
+    {
+        "Name": "Codifierz_MX6",
+        "H_username": "Codifierz_MX6"
+    },
+    {
+        "Name": "3X_MX6",
+        "H_username": "3X_MX6"
+    },
+    {
+        "Name": "Propellers_MX6",
+        "H_username": "Propellers_MX6"
+    },
+    {
+        "Name": "InterpretersLite _MX6",
+        "H_username": "InterpretersLite _MX6"
+    },
+    {
+        "Name": "SH_MX6",
+        "H_username": "SH_MX6"
+    },
+    {
+        "Name": "Task Masters_MX6",
+        "H_username": "Task Masters_MX6"
+    },
+    {
+        "Name": "Hacker_Rank_MX6",
+        "H_username": "Hacker_Rank_MX6"
+    },
+    {
+        "Name": "TrippleBlast_MX6",
+        "H_username": "TrippleBlast_MX6"
+    },
+    {
+        "Name": "teamName_MX6Cibel",
+        "H_username": "teamName_MX6Cibel"
+    },
+    {
+        "Name": "BeastMORA_MX6",
+        "H_username": "BeastMORA_MX6"
+    },
+    {
+        "Name": "TeamSync_MX6",
+        "H_username": "TeamSync_MX6"
+    },
+    {
+        "Name": "Asgardians_MX6",
+        "H_username": "Asgardians_MX6"
+    },
+    {
+        "Name": "24coders_MX6",
+        "H_username": "24coders_MX6"
+    },
+    {
+        "Name": "Cod_One _MX6",
+        "H_username": "Cod_One _MX6"
+    },
+    {
+        "Name": "Cyborgs_MX6",
+        "H_username": "Cyborgs_MX6"
+    },
+    {
+        "Name": "HIVE5_MX6",
+        "H_username": "HIVE5_MX6"
+    },
+    {
+        "Name": "hexa_MX6",
+        "H_username": "hexa_MX6"
+    },
+    {
+        "Name": "Technocrats_MX6",
+        "H_username": "Technocrats_MX6"
+    },
+    {
+        "Name": "Botz.TND_MX6",
+        "H_username": "Botz.TND_MX6"
+    },
+    {
+        "Name": "boosters_MX6",
+        "H_username": "boosters_MX6"
+    },
+    {
+        "Name": "weirdSpex_MX6",
+        "H_username": "weirdSpex_MX6"
+    }
+])}
---
+++ mail/contact.js
@@ -0,0 +1,61 @@
+
+
+
+window.addEventListener("DOMContentLoaded", function () {
+  // get the form elements defined in your form HTML above
+
+  var form = document.getElementById("my-form");
+  // var button = document.getElementById("my-form-button");
+  var status = document.getElementById("status");
+
+  // Success and Error functions for after the form is submitted
+
+  function success() {
+    form.reset();
+    status.classList.add("success");
+    status.innerHTML = "Thanks!";
+	setTimeout(fade_out, 5000);
+
+  }
+
+function fade_out() {
+  $("#status").fadeOut().empty();
+}
+
+
+  function error() {
+    status.classList.add("error");
+    status.innerHTML = "Oops! There is a problem.";
+	setTimeout(fade_out, 5000);
+  }
+ 
+
+
+  // handle the form submission event
+
+  form.addEventListener("submit", function (ev) {
+    ev.preventDefault();
+    var data = new FormData(form);
+    ajax(form.method, form.action, data, success, error);
+  });
+});
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+  var xhr = new XMLHttpRequest();
+  xhr.open(method, url);
+  xhr.setRequestHeader("Accept", "application/json");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      success(xhr.response, xhr.responseType);
+    } else {
+      error(xhr.status, xhr.response, xhr.responseType);
+    }
+  };
+  xhr.send(data);
+}
+
+
+
